ec8feb2431ac8aa5858678184406c28bfba23ed5
edem <anthonyedem46@gmail.com>
2026-01-16 12:39:00 +0100

    Exercice 3

--- TD.py
+++ TD.py
@@ -1,8 +1,6 @@
 import tkinter as tk
 import numpy as np
 
-
-
 n = 6
 size = 50
 
@@ -12,27 +10,23 @@ grid[5, 5] = 1  # première case rouge
 root = tk.Tk()
 canvas = tk.Canvas(root, width=n*size, height=n*size)
 
+isRect = True
 
 for i in range(n):
+    isRect = not isRect
     for j in range(n):
-        match i:
-            case 0 | 2 | 4:
-                color = "red"
-                canvas.create_rectangle(
+        if(isRect):
+            canvas.create_rectangle(
                     j*size, i*size,
                     (j+1)*size, (i+1)*size,
-                    fill=color, outline="black"
+                    fill='red', outline="black"
                 )
-                continue
-            case 1 | 3 | 5:
-                color = "yellow"
-                canvas.create_oval(
+        else :
+            canvas.create_oval(
                     j*size, i*size,
                     (j+1)*size, (i+1)*size,
-                    fill=color, outline="black"
+                    fill='yellow', outline="black"
                 )
-                continue
-            case _:
-                color = "white"
+        isRect = not isRect
 canvas.pack()
 root.mainloop()
